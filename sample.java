fda68c0ad0d55af88d43a5d9d09d0b40b7a74f92
Avinash Kumar Mahto <avikkumar9697@gmail.com>
2024-09-17 20:24:09 +0530

    added new things in code

--- sample.java
+++ sample.java
@@ -5,6 +5,7 @@ class sample
    System.out.println("Hello .................world");
    System.out.println("Life of pia.................");
    System.out.println("Dil se hindustani ,.......");
+   System.out.println("Dil se jharkhandi ,.......");
    
    
    
