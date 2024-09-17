04c9449053939db77a36629731eed6d71225716f
Avinash Kumar Mahto <avikkumar9697@gmail.com>
2024-09-17 20:11:45 +0530

    Initial commit

---
+++ sample.java
@@ -0,0 +1,12 @@
+class sample
+{
+ public static void main(String[] args)
+    {
+   System.out.println("Hello .................world");
+   System.out.println("Hello .................world");
+   System.out.println("Hello .................world");
+   
+   
+
+    }
+}
