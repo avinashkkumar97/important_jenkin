1201442abe2f72e8bb1a223f2cba0133505eaa18
Avinash Kumar Mahto <avikkumar9697@gmail.com>
2024-09-17 20:18:43 +0530

    added new file

--- sample.java
+++ sample.java
@@ -3,8 +3,9 @@ class sample
  public static void main(String[] args)
     {
    System.out.println("Hello .................world");
-   System.out.println("Hello .................world");
-   System.out.println("Hello .................world");
+   System.out.println("Life of pia.................");
+   System.out.println("Dil se hindustani ,.......");
+   
    
    
 
